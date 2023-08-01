dded68289cdac3455d715da2b2c6b7fc6d6942ab
penguinboots <sabrina.ynw@gmail.com>
2023-07-31 19:52:00 -0700

    build basic routes

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Landing() {
-  return(<div>Landing</div>);
+  return(<div>
+    <Link href="/home">
+      <Button>Enter</Button>
+    </Link>
+  </div>);
 }
