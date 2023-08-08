34b7746cf683de67dd9e0cecbd2585980f92dfc0
penguinboots <sabrina.ynw@gmail.com>
2023-08-08 09:55:19 -0700

    build game landing

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,10 +1,18 @@
+"use client";
+
+import Game from "@/components/Game";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Landing() {
-  return(<div>
-    <Link href="/home">
-      <Button>Enter</Button>
-    </Link>
-  </div>);
+  return (
+    <div className="h-full">
+      <Game />
+      <div className="absolute bottom-24 flex justify-center w-full p-10">
+        <Link href="/home">
+          <Button variant="secondary">Enter</Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
