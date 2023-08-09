009503a353d6d4ca04c5e38baed5288bd75b1dd3
penguinboots <sabrina.ynw@gmail.com>
2023-08-09 11:13:10 -0700

    style home page

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,14 +1,29 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Lobster } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const lobster = Lobster({ weight: ["400"], subsets: ["latin"]});
 
 const HomePage = () => {
   return (
     <div className="h-full">
-      <div className="h-full w-2/3 flex flex-col justify-center pl-12 text-3xl sm:text-4xl md:text-5xl lg:text-6xl space-y-5 font-bold">
-        <h1>hello there,</h1>
-        <h2>I&apos;m Sabrina.</h2>
+      <div className="h-full w-2/3 flex flex-col justify-center pl-12 space-y-4">
+        <h1
+          className={cn(
+            "text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-bold",
+            lobster.className
+          )}
+        >
+          Hello there,
+          <br />
+          I&apos;m Sabrina.
+        </h1>
+        <p className="text-sm sm:text-xl">
+          Developer, artist, and maker of weird things.
+        </p>
         <Link href="/experience">
-          <Button className="w-48">GET TO KNOW ME</Button>
+          <Button className="w-48 font-bold">GET TO KNOW ME</Button>
         </Link>
       </div>
     </div>
