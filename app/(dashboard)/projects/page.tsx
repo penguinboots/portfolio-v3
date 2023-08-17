e8e1bd06b7cdfe0d1703ce5bd2da1fdfbd0f546d
penguinboots <sabrina.ynw@gmail.com>
2023-08-17 10:59:47 -0700

    build projects page layout

--- app/(dashboard)/projects/page.tsx
+++ app/(dashboard)/projects/page.tsx
@@ -1,7 +1,23 @@
+import FeatureCard from "@/components/FeatureCard";
+import ProjectCard from "@/components/ProjectCard";
+
 const ProjectsPage = () => {
   return (
-    <div>ALL PROJECTS</div>
-  )
-}
+    <div>
+      <div>FEATURED</div>
+      <div className="p-4 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+        <FeatureCard />
+        <FeatureCard />
+        <FeatureCard />
+      </div>
+      <div>ALL</div>
+      <div className="p-4 gap-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </div>
+    </div>
+  );
+};
 
-export default ProjectsPage
+export default ProjectsPage;
