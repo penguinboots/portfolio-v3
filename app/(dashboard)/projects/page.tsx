ef75964d2ba6645f6dacb33139dc44ad5d1791db
penguinboots <sabrina.ynw@gmail.com>
2023-08-17 11:02:34 -0700

    add placeholders

--- app/(dashboard)/projects/page.tsx
+++ app/(dashboard)/projects/page.tsx
@@ -4,14 +4,19 @@ import ProjectCard from "@/components/ProjectCard";
 const ProjectsPage = () => {
   return (
     <div>
-      <div>FEATURED</div>
       <div className="p-4 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
         <FeatureCard />
         <FeatureCard />
         <FeatureCard />
       </div>
-      <div>ALL</div>
       <div className="p-4 gap-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
