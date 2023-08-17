7253bec557bc99d3423dfb698421e1c978a33a83
penguinboots <sabrina.ynw@gmail.com>
2023-08-17 10:59:33 -0700

    build feature component

---
+++ components/FeatureCard.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import Card from "./Card";
+import { titleFont } from "@/lib/fonts";
+
+const FeatureCard = () => {
+  return (
+    <Card>
+      <div>
+        <div className="w-full bg-slate-500 h-48 md:h-64">Image</div>
+        <div className="w-full bg-slate-200 h-48 md:h-64">
+          <h1 className={cn("text-xl", titleFont.className)}>Project Title</h1>
+          <p>Project Description</p>
+          <div>Stack</div>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default FeatureCard;
