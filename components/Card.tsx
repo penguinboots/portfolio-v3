29cf75baf41b8506fea2490c7f45049c90c806b2
penguinboots <sabrina.ynw@gmail.com>
2023-08-17 16:48:17 -0700

    fix: height/widths

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,7 +3,7 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ children }) => {
-  return <div className="bg-slate-200 rounded-xl overflow-hidden flex flex-col">{children}</div>;
+  return <div className="bg-slate-200 rounded-xl overflow-hidden flex flex-col h-full w-full">{children}</div>;
 };
 
 export default Card;
