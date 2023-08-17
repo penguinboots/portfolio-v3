4119dbad874ef08d34d4725d59f45d23d08da902
penguinboots <sabrina.ynw@gmail.com>
2023-08-17 11:07:21 -0700

    style project card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,13 +1,15 @@
 import { cn } from "@/lib/utils";
 import Card from "./Card";
 import { titleFont } from "@/lib/fonts";
+import { AiFillGithub } from "react-icons/ai";
 
 const ProjectCard = () => {
   return (
     <Card>
-      <div className="w-full h-48 flex items-center justify-center">
-        <div className="">
-          <h1 className={cn("text-xl", titleFont.className)}>Project Title</h1>
+      <div className="w-full h-48 flex flex-col items-center justify-center">
+        <h1 className={cn("text-xl", titleFont.className)}>Project Title</h1>
+        <div>
+          <AiFillGithub className="h-8 w-8" />
         </div>
       </div>
     </Card>
