45f78d955c5df4670ff965bfc15c5e3f7c7a0bf8
penguinboots <sabrina.ynw@gmail.com>
2023-08-17 16:48:05 -0700

    build project view

--- app/(dashboard)/projects/[id]/page.tsx
+++ app/(dashboard)/projects/[id]/page.tsx
@@ -1,7 +1,17 @@
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+
 const ProjectPage = () => {
   return (
-    <div>INDIVIDUAL PROJECT</div>
-  )
-}
+    <div className="flex h-full">
+      <div className="bg-slate-100 h-full flex items-center justify-center">
+        <Link href="/projects">
+          <ChevronLeft className="w-12 h-12" />
+        </Link>
+      </div>
+      <div></div>
+    </div>
+  );
+};
 
-export default ProjectPage
+export default ProjectPage;
