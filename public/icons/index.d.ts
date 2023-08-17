0371f0f88a0f23dbf60cd5e8647bcc6b257a4539
penguinboots <sabrina.ynw@gmail.com>
2023-08-17 11:13:14 -0700

    add dev icons

---
+++ public/icons/index.d.ts
@@ -0,0 +1,26 @@
+declare module "icons" {
+  interface DevIcons {
+    css: string;
+    express: string;
+    html: string;
+    jquery: string;
+    js: string;
+    node: string;
+    rails: string;
+    react: string;
+    ruby: string;
+    sass: string;
+    postgresql: string;
+    git: string;
+    prisma: string;
+    mocha: string;
+    chai: string;
+    storybook: string;
+    jest: string;
+    cypress: string;
+    rspec: string;
+    next: string;
+  }
+  const devIcons: DevIcons;
+  export = devIcons;
+}
---
+++ public/icons/index.ts
@@ -0,0 +1,53 @@
+import css from "./dev_css3.svg";
+import express from "./dev_express.svg";
+import html from "./dev_html.svg";
+import jquery from "./dev_jquery.svg";
+import js from "./dev_js.svg";
+import node from "./dev_node.svg";
+import rails from "./dev_rails.svg";
+import react from "./dev_react.svg";
+import ruby from "./dev_ruby.svg";
+import sass from "./dev_sass.svg";
+import postgresql from "./dev_postgresql.svg";
+import git from "./dev_git.svg";
+import prisma from "./dev_prisma.svg";
+import mocha from "./dev_mocha.svg";
+import chai from "./dev_chai.svg";
+import storybook from "./dev_storybook.svg";
+import jest from "./dev_jest.svg";
+import cypress from "./dev_cypress.svg";
+import rspec from "./dev_rspec.svg";
+import next from "./dev_next.svg";
+import java from "./dev_java.svg";
+import ts from "./dev_ts.svg";
+import tw from "./dev_tw.svg";
+import vercel from "./dev_vercel.svg";
+import ps from "./dev_ps.svg";
+
+export const devIcons = {
+  css,
+  express,
+  html,
+  jquery,
+  js,
+  node,
+  rails,
+  react,
+  ruby,
+  sass,
+  postgresql,
+  git,
+  prisma,
+  mocha,
+  chai,
+  storybook,
+  jest,
+  cypress,
+  rspec,
+  next,
+  java,
+  ts,
+  tw,
+  vercel,
+  ps
+};
