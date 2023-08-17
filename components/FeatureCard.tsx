da54b897a4bb79ec31ac9b16548df548f168b375
penguinboots <sabrina.ynw@gmail.com>
2023-08-17 11:13:07 -0700

    style projects

--- components/FeatureCard.tsx
+++ components/FeatureCard.tsx
@@ -7,7 +7,7 @@ const FeatureCard = () => {
     <Card>
       <div>
         <div className="w-full bg-slate-500 h-48 md:h-64">Image</div>
-        <div className="w-full bg-slate-200 h-48 md:h-64">
+        <div className="w-full p-6 bg-slate-200 h-48 md:h-64">
           <h1 className={cn("text-xl", titleFont.className)}>Project Title</h1>
           <p>Project Description</p>
           <div>Stack</div>
