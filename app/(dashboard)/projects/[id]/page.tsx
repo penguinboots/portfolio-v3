276f59840436e6c31944c7e92355cb4a7b04761b
penguinboots <sabrina.ynw@gmail.com>
2023-08-17 17:58:14 -0700

    build project md view

--- app/(dashboard)/projects/[id]/page.tsx
+++ app/(dashboard)/projects/[id]/page.tsx
@@ -1,15 +1,21 @@
+"use client";
+import MarkdownRenderer from "@/components/MarkdownRenderer";
 import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
 
 const ProjectPage = () => {
   return (
     <div className="flex h-full">
-      <div className="bg-slate-100 h-full flex items-center justify-center">
+      <div className="hidden md:flex bg-slate-100 h-full items-center justify-center">
         <Link href="/projects">
           <ChevronLeft className="w-12 h-12" />
         </Link>
       </div>
-      <div></div>
+      <div className="bg-slate-300 h-full w-full">
+        <div className="remove-all">
+          <MarkdownRenderer filePath="test.md" />
+        </div>
+      </div>
     </div>
   );
 };
