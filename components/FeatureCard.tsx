e8c81b6eced9e76ef4348f9d31524876e077c5e7
penguinboots <sabrina.ynw@gmail.com>
2023-08-18 10:53:33 -0700

    build project feature card

--- components/FeatureCard.tsx
+++ components/FeatureCard.tsx
@@ -1,20 +1,34 @@
+import Image from "next/image";
+
 import { cn } from "@/lib/utils";
-import Card from "./Card";
 import { titleFont } from "@/lib/fonts";
+import { Project } from "@/data/projects";
+
+import Card from "./Card";
 import Link from "next/link";
 
-const FeatureCard = () => {
+interface FeatureCardProps {
+  project: Project;
+}
+
+const FeatureCard: React.FC<FeatureCardProps> = ({ project }) => {
+  const stack = project.stack.map((item) => {
+    return <li key={item}>{item}</li>;
+  });
+
   return (
     <Card>
       <Link href="/projects/1">
         <div>
-          <div className="w-full bg-slate-500 h-48 md:h-64">Image</div>
+          <div className="w-full bg-slate-500 h-48 md:h-64 overflow-hidden">
+            {project.image && <Image alt="project image" src={project.image} />}
+          </div>
           <div className="w-full p-6 bg-slate-200 h-48 md:h-64">
             <h1 className={cn("text-xl", titleFont.className)}>
-              Project Title
+              {project.title}
             </h1>
-            <p>Project Description</p>
-            <div>Stack</div>
+            <p>{project.description}</p>
+            <ul className="flex gap-2">{stack}</ul>
           </div>
         </div>
       </Link>
