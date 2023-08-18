b231c1276d5d794b9d604c58eb5c8321c50355ad
penguinboots <sabrina.ynw@gmail.com>
2023-08-17 17:45:00 -0700

    build markdown renderer

---
+++ components/MarkdownRenderer.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+
+interface MarkdownRendererProps {
+  filePath: string;
+}
+
+const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ filePath }) => {
+  const [markdownContent, setMarkdownContent] = useState("");
+
+  useEffect(() => {
+    fetch(`/md/${filePath}`)
+      .then((response) => response.text())
+      .then((text) => {
+        setMarkdownContent(text);
+        console.log(typeof text)
+      });
+  }, [filePath]);
+
+  return <ReactMarkdown>{markdownContent}</ReactMarkdown>;
+};
+
+export default MarkdownRenderer;
