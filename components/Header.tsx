ab4354488e670fd52695c57f72600918b7a7a501
penguinboots <sabrina.ynw@gmail.com>
2023-08-17 17:58:19 -0700

    move navbar into header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import { LucideIcon } from "lucide-react";
 import { titleFont } from "@/lib/fonts";
+import Navbar from "./Navbar";
 
 interface HeaderProps {
   title: string;
@@ -9,11 +10,16 @@ interface HeaderProps {
 
 export const Header = ({ title, desc, icon: Icon }: HeaderProps) => {
   return (
-    <div className="bg-slate-500 h-24 flex items-center space-x-4 p-4">
-      <Icon className="w-10 h-10" />
-      <div>
-        <h1 className={titleFont.className}>{title}</h1>
-        <p>{desc}</p>
+    <div className="bg-slate-500 h-24 flex items-center justify-between p-4">
+      <div className="flex space-x-4">
+        <Icon className="w-10 h-10" />
+        <div>
+          <h1 className={titleFont.className}>{title}</h1>
+          <p>{desc}</p>
+        </div>
+      </div>
+      <div className="md:hidden">
+        <Navbar />
       </div>
     </div>
   );
