b37c5cf60daf38a7b8913bed27fee3408ce9df8c
penguinboots <sabrina.ynw@gmail.com>
2023-08-18 09:56:03 -0700

    style project overview

--- app/(dashboard)/projects/page.tsx
+++ app/(dashboard)/projects/page.tsx
@@ -3,13 +3,14 @@ import ProjectCard from "@/components/ProjectCard";
 
 const ProjectsPage = () => {
   return (
-    <div>
-      <div className="p-4 gap-4 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3">
+    <div className="w-full flex flex-col items-center">
+      <div className="w-full p-4 gap-4 grid grid-cols-1 sm:max-w-md lg:max-w-none lg:grid-cols-2 2xl:grid-cols-3">
         <FeatureCard />
         <FeatureCard />
         <FeatureCard />
+        <div className="bg-slate-100 hidden lg:flex xl:hidden">IMAGE HERE</div>
       </div>
-      <div className="p-4 gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      <div className="w-full p-4 gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
