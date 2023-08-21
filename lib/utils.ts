00a2f9d52394f6565d2b96c26c6906eac7dab4c6
penguinboots <sabrina.ynw@gmail.com>
2023-08-21 12:27:08 -0700

    add api call to sendEmail function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,23 @@
 import { ContactFormData } from "@/components/Contact";
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function sendEmail(data: ContactFormData) {
-  console.log(data);
+  const apiEndpoint = "/api/email";
+
+  fetch(apiEndpoint, {
+    method: "POST",
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      alert(response.message);
+    })
+    .catch((err) => {
+      alert(err);
+    });
 }
