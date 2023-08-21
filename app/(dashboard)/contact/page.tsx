714aaa374b34fef15191d22158d6bc4fd5a9e1d5
penguinboots <sabrina.ynw@gmail.com>
2023-08-21 12:13:14 -0700

    style contact page

--- app/(dashboard)/contact/page.tsx
+++ app/(dashboard)/contact/page.tsx
@@ -1,11 +1,12 @@
 import ContactForm from "@/components/Contact";
+import SectionCard from "@/components/SectionCard";
 
 const ContactPage = () => {
   return (
     <div className="w-full flex items-center justify-center sm:mt-24">
-      <div className="w-full sm:w-3/4 p-6 sm:p-12 rounded-3xl bg-slate-300">
+      <SectionCard>
         <ContactForm />
-      </div>
+      </SectionCard>
     </div>
   );
 };
