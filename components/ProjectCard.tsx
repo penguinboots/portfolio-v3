96703d92294040de14d587ea3a9c85f00676592d
penguinboots <sabrina.ynw@gmail.com>
2023-08-21 15:59:02 -0700

    remove card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -23,37 +23,35 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
     );
   });
   return (
-    <Card>
-      <div className="relative w-full min-h-min h-48 flex flex-col items-center justify-center">
-        <Image
-          alt={project.title}
-          src={`/projects/previews/${project.ref}.gif`}
-          fill
-          style={{ objectFit: "cover" }}
-          className="h-full"
-        />
-        <div
-          className="z-20 opacity-0 hover:opacity-100 transition-opacity h-full w-full bg-white/95 
+    <div className="relative w-full rounded-xl overflow-hidden min-h-min h-48 flex flex-col items-center justify-center">
+      <Image
+        alt={project.title}
+        src={`/projects/previews/${project.ref}.gif`}
+        fill
+        style={{ objectFit: "cover" }}
+        className="h-full"
+      />
+      <div
+        className="z-20 opacity-0 hover:opacity-100 transition-opacity h-full w-full bg-white/95 
         flex flex-col items-center justify-center"
-        >
-          <h1 className={cn("text-xl text-center px-2", titleFont.className)}>
-            {project.title}
-          </h1>
-          <p className="text-xs text-center py-1 px-6">{project.description}</p>
-          <div className="flex">
-            {project.live && (
-              <a href={project.live} target="_blank">
-                <BiSolidPointer className="h-8 w-8 hover:text-slate-600" />
-              </a>
-            )}
-            <a href={project.repo} target="_blank">
-              <AiFillGithub className="h-8 w-8 hover:text-slate-600" />
+      >
+        <h1 className={cn("text-xl text-center px-2", titleFont.className)}>
+          {project.title}
+        </h1>
+        <p className="text-xs text-center py-1 px-6">{project.description}</p>
+        <div className="flex">
+          {project.live && (
+            <a href={project.live} target="_blank">
+              <BiSolidPointer className="h-8 w-8 hover:text-slate-600" />
             </a>
-          </div>
-          <ul className="flex flex-wrap space-x-2 mt-6">{stack}</ul>
+          )}
+          <a href={project.repo} target="_blank">
+            <AiFillGithub className="h-8 w-8 hover:text-slate-600" />
+          </a>
         </div>
+        <ul className="flex flex-wrap space-x-2 mt-6">{stack}</ul>
       </div>
-    </Card>
+    </div>
   );
 };
 
