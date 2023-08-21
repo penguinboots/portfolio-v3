23d2e44a6b5a028e028cd1f6f952f137a0564392
penguinboots <sabrina.ynw@gmail.com>
2023-08-21 15:48:13 -0700

    clean up

--- components/FeatureCard.tsx
+++ components/FeatureCard.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 
@@ -19,51 +19,53 @@ interface FeatureCardProps {
 
 const FeatureCard: React.FC<FeatureCardProps> = ({ project }) => {
   const stack = project.stack.map((item) => {
-    return <li key={item} className="text-lg w-7 h-7"><DevIcon name={item}/></li>;
+    return (
+      <li key={item} className="text-lg w-7 h-7">
+        <DevIcon name={item} />
+      </li>
+    );
   });
 
   return (
     <Card>
-      <div>
-        <div className="relative bg-slate-400 min-h-48 h-48 md:h-64 overflow-hidden">
-          <Link href={`/projects/${project.ref}`}>
-            <Image
-              alt={project.title}
-              src={`/projects/previews/${project.ref}.gif`}
-              fill
-              style={{ objectFit: "cover" }}
-              className="hover:opacity-80 transition-opacity"
-            />
-          </Link>
-        </div>
-        <div className="flex flex-col justify-between p-6 min-h-48 bg-slate-300 md:h-64">
-          <div>
-            <div className="flex justify-between items-center py-2">
-              <Link href={`/projects/${project.ref}`}>
-                <h1
-                  className={cn(
-                    "text-xl  hover:text-slate-600",
-                    titleFont.className
-                  )}
-                >
-                  {project.title}
-                </h1>
-              </Link>
-              <div className="flex">
-                {project.live && (
-                  <a href={project.live} target="_blank">
-                    <BiSolidPointer className="h-8 w-8 hover:text-slate-600" />
-                  </a>
+      <div className="relative bg-slate-400 min-h-48 h-48 md:h-64 overflow-hidden">
+        <Link href={`/projects/${project.ref}`}>
+          <Image
+            alt={project.title}
+            src={`/projects/previews/${project.ref}.gif`}
+            fill
+            style={{ objectFit: "cover" }}
+            className="hover:opacity-80 transition-opacity"
+          />
+        </Link>
+      </div>
+      <div className="flex flex-col justify-between p-6 min-h-48 bg-slate-300 md:h-64">
+        <div>
+          <div className="flex justify-between items-center py-2">
+            <Link href={`/projects/${project.ref}`}>
+              <h1
+                className={cn(
+                  "text-xl  hover:text-slate-600",
+                  titleFont.className
                 )}
-                <a href={project.repo} target="_blank">
-                  <AiFillGithub className="h-8 w-8 hover:text-slate-600" />
+              >
+                {project.title}
+              </h1>
+            </Link>
+            <div className="flex">
+              {project.live && (
+                <a href={project.live} target="_blank">
+                  <BiSolidPointer className="h-8 w-8 hover:text-slate-600" />
                 </a>
-              </div>
+              )}
+              <a href={project.repo} target="_blank">
+                <AiFillGithub className="h-8 w-8 hover:text-slate-600" />
+              </a>
             </div>
-            <p>{project.description}</p>
           </div>
-          <ul className="flex flex-wrap gap-2">{stack}</ul>
+          <p>{project.description}</p>
         </div>
+        <ul className="flex flex-wrap gap-2">{stack}</ul>
       </div>
     </Card>
   );
