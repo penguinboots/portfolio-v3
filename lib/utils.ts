76a7260c3c7ea6684631696894b4ae2dbe7915c9
penguinboots <sabrina.ynw@gmail.com>
2023-08-21 11:40:38 -0700

    placeholder function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
+import { ContactFormData } from "@/components/Contact";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function sendEmail(data: ContactFormData) {
+  console.log(data);
 }
