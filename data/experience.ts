a49fe6fe34667d8bbb7a689889cf6cb5b7be00a5
penguinboots <sabrina.ynw@gmail.com>
2023-08-22 10:50:20 -0700

    edit experiences data

--- data/experience.ts
+++ data/experience.ts
@@ -1,37 +1,93 @@
-import { GraduationCap, LucideIcon } from "lucide-react";
+import { GraduationCap, LucideIcon, Sandwich } from "lucide-react";
+import { BiCake } from "react-icons/bi";
+import { SiApple } from "react-icons/si";
+import { GoLaw } from "react-icons/go";
+
 
 export interface Experience {
   title: string;
   note: string;
   subtitle: string;
-  completed: string;
+  date: string;
   desc: string;
   bullets: string[];
   icon: LucideIcon;
   type: string;
 }
 
-const lighthouse: Experience = {
+export const lighthouse: Experience = {
   title: "Lighthouse Labs",
   note: "",
   subtitle: "Diploma - Web Development",
-  completed: "June 2023",
+  date: "June 2023",
   desc: "",
   bullets: ["test", "test", "test"],
   icon: GraduationCap,
   type: "Education",
 };
 
-const ubc: Experience = {
+export const ubc: Experience = {
   title: "The University of British Columbia",
   note: "graduated with distinction",
   subtitle: "Bachelor of Arts - Psychology",
-  completed: "May 2023",
+  date: "May 2023",
   desc: "",
   bullets: ["test", "test", "test"],
   icon: GraduationCap,
   type: "Education",
 };
 
+export const apple: Experience = {
+  title: "Specialist",
+  note: "",
+  subtitle: "Apple",
+  date: "August 2021 - December 2021",
+  desc: "",
+  bullets: ["test", "test", "test"],
+  icon: SiApple,
+  type: "Work",
+};
 
-export { lighthouse, ubc };
+export const catch122: Experience = {
+  title: "Expeditor",
+  note: "",
+  subtitle: "Catch122 Bistro",
+  date: "February 2020 - August 2021",
+  desc: "",
+  bullets: ["test", "test", "test"],
+  icon: Sandwich,
+  type: "Work",
+};
+
+export const sangra: Experience = {
+  title: "Legal Administrative Assistant",
+  note: "",
+  subtitle: "Sangra Moller LLP",
+  date: "September 2019 - December 2019",
+  desc: "",
+  bullets: ["test", "test", "test"],
+  icon: GoLaw,
+  type: "Work",
+};
+
+export const murphy: Experience = {
+  title: "Legal Administrative Assistant",
+  note: "",
+  subtitle: "Murphy & Company LLP",
+  date: "June 2019 - September 2019",
+  desc: "",
+  bullets: ["test", "test", "test"],
+  icon: GoLaw,
+  type: "Work",
+};
+
+export const minami: Experience = {
+  title: "Pastry Cook",
+  note: "",
+  subtitle: "Minami Restaurant",
+  date: "March 2018 - November 2019",
+  desc: "",
+  bullets: ["test", "test", "test"],
+  icon: BiCake,
+  type: "Work",
+};
