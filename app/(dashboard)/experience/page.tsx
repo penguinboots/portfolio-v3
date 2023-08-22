c7bbd8449030442ac45715a7bcb69f2aa79ecd10
penguinboots <sabrina.ynw@gmail.com>
2023-08-22 10:50:05 -0700

    build experience section

--- app/(dashboard)/experience/page.tsx
+++ app/(dashboard)/experience/page.tsx
@@ -1,6 +1,13 @@
 import ExperienceCard from "@/components/ExperienceCard";
 import SectionCard from "@/components/SectionCard";
-import { lighthouse, ubc } from "@/data/experience";
+import {
+  apple,
+  catch122,
+  lighthouse,
+  minami,
+  murphy,
+  ubc,
+} from "@/data/experience";
 
 const ExperiencePage = () => {
   let education = [lighthouse, ubc];
@@ -8,11 +15,20 @@ const ExperiencePage = () => {
     <ExperienceCard key={exp.title} exp={exp} />
   ));
 
+  let work = [apple, catch122, minami, murphy];
+  let workCards = work.map((exp) => (
+    <ExperienceCard key={exp.title} exp={exp} />
+  ));
+
+  let sectionTitle = "text-bold text-2xl mt-6 mb-2 first:mt-0"
+  let cardSection = "w-full space-y-3"
+
   return (
-    <div className="w-full flex items-center justify-center sm:py-8">
-      <SectionCard colour="slate-300">
-        <div className="space-y-6">{eduCards}</div>
-      </SectionCard>
+    <div className="w-full flex flex-col p-4 sm:px-12 sm:py-8">
+      <h1 className={sectionTitle}>Education</h1>
+      <div className={cardSection}>{eduCards}</div>
+      <h1 className={sectionTitle}>Employment</h1>
+      <div className={cardSection}>{workCards}</div>
     </div>
   );
 };
