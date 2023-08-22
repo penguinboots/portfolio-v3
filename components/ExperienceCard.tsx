39f1aaca6f60d21aafd06a1da334723b4c32b1c0
penguinboots <sabrina.ynw@gmail.com>
2023-08-22 11:09:07 -0700

    style experience card

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -10,15 +10,13 @@ const ExperienceCard = ({ exp }: ExperienceCardProps) => {
   return (
     <div className="relative w-full rounded-xl overflow-hidden flex flex-col bg-slate-100 p-6">
       <exp.icon className="w-6 h-6 mb-1" />
-      <div className="flex flex-col">
+      <div className="flex flex-col mb-2">
         <p className="text-sm">{exp.date.toUpperCase()}</p>
         <h1 className="font-bold text-xl">{exp.title}</h1>
-        <h2 className="text-md">{exp.subtitle}</h2>
+        <h2 className="text-sm">{exp.subtitle.toUpperCase()}</h2>
       </div>
-      <div></div>
-      <div>
-        <ul className="list-disc list-inside pl-2">{bulletList}</ul>
-      </div>
+      <div>{exp.desc}</div>
+      <ul className="list-disc list-inside pl-2">{bulletList}</ul>
     </div>
   );
 };
