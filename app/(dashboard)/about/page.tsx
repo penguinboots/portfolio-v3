e1aec0639bf7b4cff18e7579765d7d4ea0d4e05e
penguinboots <sabrina.ynw@gmail.com>
2023-08-22 10:49:58 -0700

    add about page placeholders

--- app/(dashboard)/about/page.tsx
+++ app/(dashboard)/about/page.tsx
@@ -1,5 +1,17 @@
+import SectionCard from "@/components/SectionCard";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className="w-full flex items-center justify-center sm:py-8">
+      <SectionCard colour="slate-300">
+        <h1>Placeholder placeholder</h1>
+        <p>
+          placeholder placeholder placeholder placeholder placeholder
+          placeholder placeholder placeholder placeholder placeholder
+        </p>
+      </SectionCard>
+    </div>
+  );
 };
 
 export default AboutPage;
