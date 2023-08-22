10305b14b9cf1cd3a44e59a5a75322c349e24bb4
penguinboots <sabrina.ynw@gmail.com>
2023-08-22 10:50:11 -0700

    style experience card

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -5,19 +5,17 @@ interface ExperienceCardProps {
 }
 
 const ExperienceCard = ({ exp }: ExperienceCardProps) => {
-  
-  let bulletList = exp.bullets.map((bullet) => (<li key={bullet}>{bullet}</li>))
+  let bulletList = exp.bullets.map((bullet) => <li key={bullet}>{bullet}</li>);
 
   return (
     <div className="relative w-full rounded-xl overflow-hidden flex flex-col bg-slate-100 p-6">
-      <div className="lg:flex items-center gap-4">
+      <exp.icon className="w-6 h-6 mb-1" />
+      <div className="flex flex-col">
+        <p className="text-sm">{exp.date.toUpperCase()}</p>
         <h1 className="font-bold text-xl">{exp.title}</h1>
-        {exp.note && <p className="text-sm pt-1">{exp.note.toUpperCase()}</p>}
-      </div>
-      <div>
-        <h2>{exp.completed}</h2>
         <h2 className="text-md">{exp.subtitle}</h2>
       </div>
+      <div></div>
       <div>
         <ul className="list-disc list-inside pl-2">{bulletList}</ul>
       </div>
