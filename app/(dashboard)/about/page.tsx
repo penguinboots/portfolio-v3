67648e9f7769a890a36d1e6afc4ba39a328a121f
penguinboots <sabrina.ynw@gmail.com>
2023-08-28 13:57:50 -0700

    adjust colours

--- app/(dashboard)/about/page.tsx
+++ app/(dashboard)/about/page.tsx
@@ -3,7 +3,7 @@ import SectionCard from "@/components/SectionCard";
 const AboutPage = () => {
   return (
     <div className="w-full flex items-center justify-center sm:py-8">
-      <SectionCard colour="slate-300">
+      <SectionCard>
         <h1>Placeholder placeholder</h1>
         <p>
           placeholder placeholder placeholder placeholder placeholder
