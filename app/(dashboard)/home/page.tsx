65ca4d066bdd67b43ad0fa38dca16d6713db4a18
penguinboots <sabrina.ynw@gmail.com>
2023-08-31 10:57:54 -0700

    add motion

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,12 +1,22 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { coverFont } from "@/lib/fonts";
 
+import { motion } from "framer-motion";
+
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
   return (
     <div className="h-full text-sidebar bg-card">
-      <div className="h-full w-2/3 flex flex-col justify-center pl-12 space-y-4">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, x: 50 }}
+        transition={{ ease: "easeOut", duration: 0.5 }}
+        className="h-full w-2/3 flex flex-col justify-center pl-12 space-y-4"
+      >
         <h1
           className={cn(
             "text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-bold",
@@ -21,9 +31,11 @@ const HomePage = () => {
           Developer, artist, and maker of weird things.
         </p>
         <Link href="/about">
-          <Button className="w-48 font-bold bg-base text-header-text hover:bg-header">GET TO KNOW ME</Button>
+          <Button className="w-48 font-bold bg-base text-header-text hover:bg-header">
+            GET TO KNOW ME
+          </Button>
         </Link>
-      </div>
+      </motion.div>
     </div>
   );
 };
