d11e5db729949637e450f69e3f88cbd0a2e3ed75
penguinboots <sabrina.ynw@gmail.com>
2023-09-12 12:01:40 -0700

    add mobile menu behaviour

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,15 +6,39 @@ import { cn } from "@/lib/utils";
 import { useState } from "react";
 
 import { motion, AnimatePresence } from "framer-motion";
+import routes from "@/lib/routes";
+import type { Route } from "../lib/routes";
+import { AiFillLinkedin, AiFillGithub, AiOutlineMail } from "react-icons/ai";
 
-interface HeaderProps {
-  title: string;
-  desc: string;
-  icon: LucideIcon;
-}
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-export const Header = ({ title, desc, icon: Icon }: HeaderProps) => {
+const routeArray: Route[] = Object.values(routes);
+const socials = [
+  {
+    label: "LinkedIn",
+    icon: AiFillLinkedin,
+    href: "https://www.linkedin.com/in/sabrina-y-wang/",
+  },
+  {
+    label: "Github",
+    icon: AiFillGithub,
+    href: "https://github.com/penguinboots",
+  },
+  {
+    label: "Email",
+    icon: AiOutlineMail,
+    href: "mailto: sabrina.ynw@gmail.com",
+  },
+];
+
+export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  let title = routes[pathname].label;
+  let desc = routes[pathname].description;
+  let Icon = routes[pathname].icon;
 
   return (
     <div
@@ -24,7 +48,12 @@ export const Header = ({ title, desc, icon: Icon }: HeaderProps) => {
       )}
     >
       <div className="flex">
-        <div className="flex items-center h-24 p-6">
+        <div
+          className={cn(
+            "flex items-center h-24 p-6 transition-all duration-500",
+            isOpen ? "opacity-0" : "opacity-100"
+          )}
+        >
           <Icon className="w-12 h-12" />
           <div>
             <h1 className={cn("text-xl pt-1", titleFont.className)}>{title}</h1>
@@ -40,11 +69,30 @@ export const Header = ({ title, desc, icon: Icon }: HeaderProps) => {
           <motion.div
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: 50 }}
+            exit={{ opacity: 0, x: -50 }}
             transition={{ ease: "linear", duration: 0.5 }}
-            className="w-full flex flex-col items-center sm:pb-10 md:px-12"
+            className="w-full flex flex-col items-center sm:pb-10 md:px-12 z-30"
           >
-            
+            {routeArray.map((route) => (
+              <Link
+                href={route.href}
+                key={route.href}
+                className={cn(
+                  `
+                  text-xl flex justify-center p-6 w-2/3
+                  font-medium cursor-pointer
+                  hover:text-sidebar-text-hover hover:bg-white/10
+                  rounded-lg transition-all`,
+                  pathname.includes(route.href)
+                    ? "text-sidebar-text bg-white/10"
+                    : "text-sidebar-text"
+                )}
+                onClick={() => setIsOpen(false)}
+              >
+                <route.icon className="h-6 w-6 mr-1" />
+                {route.label}
+              </Link>
+            ))}
           </motion.div>
         )}
       </AnimatePresence>
