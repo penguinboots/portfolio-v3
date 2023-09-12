6deaf7b26c86965406928bf5f4e12fa4807d5a05
penguinboots <sabrina.ynw@gmail.com>
2023-09-12 16:16:05 -0700

    add motion to project detail view

--- app/(dashboard)/projects/[id]/page.tsx
+++ app/(dashboard)/projects/[id]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { motion, AnimatePresence } from "framer-motion";
 import MarkdownRenderer from "@/components/MarkdownRenderer";
 import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
@@ -9,27 +10,56 @@ const ProjectPage = () => {
   const projectRef = pathname.substring(pathname.lastIndexOf("/"));
 
   return (
-    <div className="flex h-auto bg-white px-4">
-      <div className="fixed hidden h-full items-center md:flex">
-        <Link href="/projects">
-          <div className="flex items-center justify-center rounded-2xl bg-slate-200 px-1 py-4">
-            <ChevronLeft className="h-12 w-12" />
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1 }}
+        className="flex h-auto bg-white px-4"
+      >
+        {/* DESKTOP BACK */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          className="fixed hidden h-full items-center md:flex"
+        >
+          <Link href="/projects">
+            <div className="flex items-center justify-center rounded-2xl bg-slate-200 px-1 py-4">
+              <ChevronLeft className="h-12 w-12" />
+            </div>
+          </Link>
+        </motion.div>
+        {/* MOBILE BACK */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          className="fixed bottom-4 left-4 z-30 flex md:hidden"
+        >
+          <Link href="/projects">
+            <div className="rounded-full bg-slate-200 p-2">
+              <ChevronLeft className="h-12 w-12" />
+            </div>
+          </Link>
+        </motion.div>
+        {/* MARKDOWN */}
+        <motion.div
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 50 }}
+          transition={{ duration: 0.75 }}
+          className="flex w-full flex-col items-center"
+        >
+          <div className="md:w-3/4 md:py-12 lg:w-2/3">
+            <MarkdownRenderer filePath={`${projectRef}.md`} />
           </div>
-        </Link>
-      </div>
-      <div className="fixed bottom-4 left-4 flex md:hidden">
-        <Link href="/projects">
-          <div className="rounded-full bg-slate-200 p-2">
-            <ChevronLeft className="h-12 w-12" />
-          </div>
-        </Link>
-      </div>
-      <div className="flex w-full flex-col items-center">
-        <div className="md:w-3/4 md:py-12 lg:w-2/3">
-          <MarkdownRenderer filePath={`${projectRef}.md`} />
-        </div>
-      </div>
-    </div>
+        </motion.div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
