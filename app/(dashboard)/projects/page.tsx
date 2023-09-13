dd9819fc9cab359d99796bb38241aae512d1b190
penguinboots <sabrina.ynw@gmail.com>
2023-09-13 12:40:17 -0700

    add stagger transition

--- app/(dashboard)/projects/page.tsx
+++ app/(dashboard)/projects/page.tsx
@@ -37,17 +37,32 @@ const ProjectsPage = () => {
       transition={{ ease: "easeOut", duration: 0.75 }}
       className="flex w-full flex-col items-center"
     >
-      <div className="grid w-full grid-cols-1 gap-4 p-4 sm:max-w-md lg:max-w-none lg:grid-cols-2 2xl:grid-cols-3">
-        {features.map((feature) => (
-          <FeatureCard key={feature.title} project={feature} />
+      <ul className="grid w-full grid-cols-1 gap-4 p-4 sm:max-w-md lg:max-w-none lg:grid-cols-2 2xl:grid-cols-3">
+        {features.map((feature, i) => (
+          <motion.li
+            initial={{ opacity: 0.5, y: -40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.5 / features.length * i }}
+            key={feature.title}
+            className="list-none"
+          >
+            <FeatureCard project={feature} />
+          </motion.li>
         ))}
-        <div className="hidden bg-slate-100 lg:flex xl:hidden">IMAGE HERE</div>
-      </div>
-      <div className="grid w-full grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {projects.map((project) => (
-          <ProjectCard key={project.title} project={project} />
+        <li className="hidden bg-slate-100 lg:flex xl:hidden">IMAGE HERE</li>
+      </ul>
+      <ul className="grid w-full list-none grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {projects.map((project, i) => (
+          <motion.li
+            initial={{ opacity: 0.5, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, delay: 0.5 / projects.length * i }}
+            key={project.title}
+          >
+            <ProjectCard project={project} />
+          </motion.li>
         ))}
-      </div>
+      </ul>
     </motion.div>
   );
 };
