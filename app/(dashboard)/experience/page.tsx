b3e50f27f4a24ffa385168c6a5cd5694c4c1d3e3
penguinboots <sabrina.ynw@gmail.com>
2023-09-13 13:08:58 -0700

    animate experience page

--- app/(dashboard)/experience/page.tsx
+++ app/(dashboard)/experience/page.tsx
@@ -15,17 +15,31 @@ const education = [lighthouse, ubc];
 const work = [apple, catch122, minami, murphy];
 
 const ExperiencePage = () => {
-  let eduCards = education.map((exp) => (
-    <ExperienceCard key={exp.title} exp={exp} />
+  let eduCards = education.map((exp, i) => (
+    <motion.div
+      initial={{ opacity: 0.5, y: -40 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, delay: (0.5 / education.length) * i }}
+      key={exp.title}
+      className=""
+    >
+      <ExperienceCard exp={exp} />
+    </motion.div>
   ));
 
-  let workCards = work.map((exp) => (
-    <ExperienceCard key={exp.title} exp={exp} />
+  let workCards = work.map((exp, i) => (
+    <motion.div
+      initial={{ opacity: 0.5, y: -40 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, delay: (0.5 / work.length) * i }}
+      key={exp.title}
+    >
+      <ExperienceCard exp={exp} />
+    </motion.div>
   ));
 
   let sectionTitle =
     "w-full lg:w-5/6 text-bold text-2xl mt-6 mb-2 px-2 md:px-0 md:mt-8";
-  let cardSection = "flex flex-col items-center w-full space-y-3";
 
   return (
     <motion.div
@@ -35,9 +49,29 @@ const ExperiencePage = () => {
       className="flex w-full flex-col items-center sm:pb-10 md:px-12"
     >
       <h1 className={sectionTitle}>Education</h1>
-      <div className={cardSection}>{eduCards}</div>
+      {education.map((exp, i) => (
+        <motion.div
+          initial={{ opacity: 0, x: -30 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0.15 * i }}
+          key={exp.title}
+          className="flex w-full flex-col items-center py-2"
+        >
+          <ExperienceCard exp={exp} />
+        </motion.div>
+      ))}
       <h1 className={sectionTitle}>Employment</h1>
-      <div className={cardSection}>{workCards}</div>
+      {work.map((exp, i) => (
+        <motion.div
+          initial={{ opacity: 0, x: -30 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0.15 * i }}
+          key={exp.title}
+          className="flex w-full flex-col items-center py-2"
+        >
+          <ExperienceCard exp={exp} />
+        </motion.div>
+      ))}
     </motion.div>
   );
 };
