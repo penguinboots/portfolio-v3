012122ed3414d3b6a0d909732dae194843a7aee2
penguinboots <sabrina.ynw@gmail.com>
2023-09-13 18:55:31 -0700

    updatel link

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -41,7 +41,7 @@ const HomePage = () => {
           Developer, artist, and maker of weird things.
         </p>
         <Button className="w-48 bg-base font-bold text-header-text hover:bg-header">
-          <Link href="/about" className="w-auto">
+          <Link href="/projects" className="w-auto">
             GET TO KNOW ME
           </Link>
         </Button>
